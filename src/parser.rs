8ac6cb5dd7b78dbec1a2ca0421ad137733e2c972
9506hqwy <97424592+9506hqwy@users.noreply.github.com>
2025-10-05 16:17:56 +0900

    nom を 8 に更新した。

--- src/parser.rs
+++ src/parser.rs
@@ -1,19 +1,19 @@
 use super::error::Error;
 use super::ids::{BaseClass, Device, ProgIf, SubClass, SubSystem, Vendor};
 use nom::{
-    IResult,
+    IResult, Parser,
     branch::alt,
     bytes::complete::{tag, take_until, take_until1},
     character::complete::hex_digit1,
     combinator::map,
     multi::{many0, many1},
-    sequence::{preceded, tuple},
+    sequence::preceded,
 };
 
 pub fn parse(input: &str) -> Result<(Vec<Vendor>, Vec<BaseClass>), Error> {
-    let (rest, vencors) = many1(vendor_block)(input)?;
-    let (rest, classes) = many1(class_block)(rest)?;
-    let (rest, _) = many0(tag("\n"))(rest)?;
+    let (rest, vencors) = many1(vendor_block).parse(input)?;
+    let (rest, classes) = many1(class_block).parse(rest)?;
+    let (rest, _) = many0(tag("\n")).parse(rest)?;
     if !rest.is_empty() {
         return Err(Error::TrailingData);
     }
@@ -24,129 +24,125 @@ fn vendor_id(input: &str) -> IResult<&str, u16> {
     map(
         preceded(many0(alt((tag("\n"), comment))), hex_digit1),
         |v: &str| u16::from_str_radix(v, 16).unwrap(),
-    )(input)
+    )
+    .parse(input)
 }
 
 fn device_id(input: &str) -> IResult<&str, u16> {
     map(
-        preceded(
-            tuple((many0(alt((tag("\n"), comment))), tag("\t"))),
-            hex_digit1,
-        ),
+        preceded((many0(alt((tag("\n"), comment))), tag("\t")), hex_digit1),
         |v: &str| u16::from_str_radix(v, 16).unwrap(),
-    )(input)
+    )
+    .parse(input)
 }
 
 fn subvendor_id(input: &str) -> IResult<&str, u16> {
     map(
-        preceded(
-            tuple((many0(alt((tag("\n"), comment))), tag("\t\t"))),
-            hex_digit1,
-        ),
+        preceded((many0(alt((tag("\n"), comment))), tag("\t\t")), hex_digit1),
         |v: &str| u16::from_str_radix(v, 16).unwrap(),
-    )(input)
+    )
+    .parse(input)
 }
 
 fn subdevice_id(input: &str) -> IResult<&str, u16> {
-    map(hex_digit1, |v: &str| u16::from_str_radix(v, 16).unwrap())(input)
+    map(hex_digit1, |v: &str| u16::from_str_radix(v, 16).unwrap()).parse(input)
 }
 
 fn class_id(input: &str) -> IResult<&str, u8> {
     map(
-        preceded(
-            tuple((many0(alt((tag("\n"), comment))), tag("C "))),
-            hex_digit1,
-        ),
+        preceded((many0(alt((tag("\n"), comment))), tag("C ")), hex_digit1),
         |v: &str| u8::from_str_radix(v, 16).unwrap(),
-    )(input)
+    )
+    .parse(input)
 }
 
 fn subclass_id(input: &str) -> IResult<&str, u8> {
     map(
-        preceded(
-            tuple((many0(alt((tag("\n"), comment))), tag("\t"))),
-            hex_digit1,
-        ),
+        preceded((many0(alt((tag("\n"), comment))), tag("\t")), hex_digit1),
         |v: &str| u8::from_str_radix(v, 16).unwrap(),
-    )(input)
+    )
+    .parse(input)
 }
 
 fn progif_id(input: &str) -> IResult<&str, u8> {
     map(
-        preceded(
-            tuple((many0(alt((tag("\n"), comment))), tag("\t\t"))),
-            hex_digit1,
-        ),
+        preceded((many0(alt((tag("\n"), comment))), tag("\t\t")), hex_digit1),
         |v: &str| u8::from_str_radix(v, 16).unwrap(),
-    )(input)
+    )
+    .parse(input)
 }
 
 fn str_to_eol(input: &str) -> IResult<&str, &str> {
-    take_until1("\n")(input)
+    take_until1("\n").parse(input)
 }
 
 fn comment(input: &str) -> IResult<&str, &str> {
-    map(tuple((tag("#"), take_until("\n"))), |(_, _)| "")(input)
+    map((tag("#"), take_until("\n")), |(_, _)| "").parse(input)
 }
 
 fn vendor_block(input: &str) -> IResult<&str, Vendor> {
     map(
-        tuple((
+        (
             vendor_id,
             preceded(tag("  "), str_to_eol),
             many0(device_block),
-        )),
+        ),
         |(i, n, d)| Vendor::new(i, n.to_string(), d),
-    )(input)
+    )
+    .parse(input)
 }
 
 fn device_block(input: &str) -> IResult<&str, Device> {
     map(
-        tuple((
+        (
             device_id,
             preceded(tag("  "), str_to_eol),
             many0(subsystem_block),
-        )),
+        ),
         |(i, n, s)| Device::new(i, n.to_string(), s),
-    )(input)
+    )
+    .parse(input)
 }
 
 fn subsystem_block(input: &str) -> IResult<&str, SubSystem> {
     map(
-        tuple((
+        (
             subvendor_id,
             preceded(tag(" "), subdevice_id),
             preceded(tag("  "), str_to_eol),
-        )),
+        ),
         |(v, d, n)| SubSystem::new(v, d, n.to_string()),
-    )(input)
+    )
+    .parse(input)
 }
 
 fn class_block(input: &str) -> IResult<&str, BaseClass> {
     map(
-        tuple((
+        (
             class_id,
             preceded(tag("  "), str_to_eol),
             many0(subclass_block),
-        )),
+        ),
         |(i, n, s)| BaseClass::new(i, n.to_string(), s),
-    )(input)
+    )
+    .parse(input)
 }
 
 fn subclass_block(input: &str) -> IResult<&str, SubClass> {
     map(
-        tuple((
+        (
             subclass_id,
             preceded(tag("  "), str_to_eol),
             many0(progif_block),
-        )),
+        ),
         |(i, n, p)| SubClass::new(i, n.to_string(), p),
-    )(input)
+    )
+    .parse(input)
 }
 
 fn progif_block(input: &str) -> IResult<&str, ProgIf> {
-    map(
-        tuple((progif_id, preceded(tag("  "), str_to_eol))),
-        |(i, n)| ProgIf::new(i, n.to_string()),
-    )(input)
+    map((progif_id, preceded(tag("  "), str_to_eol)), |(i, n)| {
+        ProgIf::new(i, n.to_string())
+    })
+    .parse(input)
 }
